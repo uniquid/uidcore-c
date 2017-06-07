09e38809e9864d1415563d84bd1704acf11b520e
Marco Palumbi <marco@palumbi.it>
2017-06-07 18:57:02 +0200

    Added tests for functions in UID_dispatch and UID_message modules

--- tests.c
+++ tests.c
@@ -4,9 +4,15 @@
 //#include "CUnit/Console.h"
 
 #include "UID_identity.h"
+#include "UID_bchainBTC.h"
+#include "UID_message.h"
+#include "UID_dispatch.h"
 
 #include <stdio.h>  // for printf
 #include <unistd.h> // unlink
+
+
+/**************************** Identity test suite *******************************/
 
 /* Test Suite setup and cleanup functions: */
 
@@ -14,19 +20,6 @@ int init_identity_suite(void)  { return 0; }
 int clean_identity_suite(void) { return 0; }
 
 /************* Test case functions ****************/
-
-void test_case_sample(void)
-{
-   CU_ASSERT(CU_TRUE);
-   CU_ASSERT_NOT_EQUAL(2, -1);
-   CU_ASSERT_STRING_EQUAL("string #1", "string #1");
-   CU_ASSERT_STRING_NOT_EQUAL("string #1", "string #2");
-
-   CU_ASSERT(CU_FALSE);
-   CU_ASSERT_EQUAL(2, 3);
-   CU_ASSERT_STRING_NOT_EQUAL("string #1", "string #1");
-   CU_ASSERT_STRING_EQUAL("string #1", "string #2");
-}
 
 void test_case_identity1(void)
 {
@@ -140,6 +133,126 @@ void test_case_identity4(void)
 
 	unlink("identity.db");
 }
+
+/**************************** General test suite *******************************/
+
+/* Test Suite setup and cleanup functions: */
+
+int init_general_suite(void)
+{
+	extern cache_buffer *current;
+	uint8_t bit_mask1[18] = { 0x00, 0x00, 0x00, 0x80     }; // bit 31 ON
+
+    strncpy(current->contractsCache[0].serviceUserAddress, "my3CohS9f57yCqNy4yAPbBRqLaAAJ9oqXV", sizeof(BTC_Address));          // user     m/0'/0/0
+    strncpy(current->contractsCache[0].serviceProviderAddress, "mw5oLLjxSNsPRdDgArCZseGEQJVdNYNK5U", sizeof(BTC_Address));      // provider m/0'/0/0
+    memcpy(current->contractsCache[0].profile.bit_mask, bit_mask1, sizeof(current->contractsCache[0].profile.bit_mask));
+    strncpy(current->contractsCache[1].serviceUserAddress, "myUFCeVGwkJv3PXy4zc1KSWRT8dC5iTvhU", sizeof(BTC_Address));          // user1    m/0'/0/1
+    strncpy(current->contractsCache[1].serviceProviderAddress, "mtEQ22KCcjpz73hWfNvJoq6tqMEcRUKk3m", sizeof(BTC_Address));      // provider m/0'/0/1
+    memset(current->contractsCache[1].profile.bit_mask, 0, sizeof(current->contractsCache[1].profile.bit_mask));
+    current->validCacheEntries = 2;
+    strncpy(current->clientCache[0].serviceProviderName, "LocalMachine", sizeof(((UID_ClientProfile *)0)->serviceProviderName));
+    strncpy(current->clientCache[0].serviceProviderAddress, "mw5oLLjxSNsPRdDgArCZseGEQJVdNYNK5U", sizeof(((UID_ClientProfile *)0)->serviceProviderAddress));// provider m/0'/0/0
+    strncpy(current->clientCache[0].serviceUserAddress, "my3CohS9f57yCqNy4yAPbBRqLaAAJ9oqXV", sizeof(((UID_ClientProfile *)0)->serviceUserAddress));        // user     m/0'/0/0
+    strncpy(current->clientCache[1].serviceProviderName, "UID984fee057c6d", sizeof(((UID_ClientProfile *)0)->serviceProviderName));
+    strncpy(current->clientCache[1].serviceProviderAddress, "mtEQ22KCcjpz73hWfNvJoq6tqMEcRUKk3m", sizeof(((UID_ClientProfile *)0)->serviceProviderAddress));// provider m/0'/0/1
+    strncpy(current->clientCache[1].serviceUserAddress, "myUFCeVGwkJv3PXy4zc1KSWRT8dC5iTvhU", sizeof(((UID_ClientProfile *)0)->serviceUserAddress));        // user1     m/0'/0/1
+    strncpy(current->clientCache[2].serviceProviderName, "nocontract", sizeof(((UID_ClientProfile *)0)->serviceProviderName));
+    strncpy(current->clientCache[2].serviceProviderAddress, "mtEQ22KCcjpz73hWfNvJoq6tqMEcRUKk3m", sizeof(((UID_ClientProfile *)0)->serviceProviderAddress));// provider m/0'/0/1
+    strncpy(current->clientCache[2].serviceUserAddress, "n1UevZASvVyNhAB2d5Nm9EaHFeooJZbSP7", sizeof(((UID_ClientProfile *)0)->serviceUserAddress));        // user1     m/0'/0/3
+    current->validClientEntries = 3;
+
+
+	unlink("identity.db");
+	UID_getLocalIdentity("tprv8ZgxMBicQKsPdoj3tQG8Z2bzNsCTsk9heayJQA1pQStVx2hLEyVwx6gfHZ2p4dSzbvaEw7qrDXnX54vTVbkLghZcB24TXuj1ADXPUCvyfcy");
+	return 0;
+}
+int clean_general_suite(void)
+{
+	unlink("identity.db");
+	return 0;
+}
+
+/************* Test case functions ****************/
+
+void test_case_general1(void)
+{
+	UID_ClientChannelCtx u_ctx;
+
+	// user
+	CU_ASSERT_NOT_EQUAL(0, UID_createChannel("noName", &u_ctx));
+	CU_ASSERT_EQUAL(0, UID_createChannel("LocalMachine", &u_ctx));
+	CU_ASSERT_STRING_EQUAL("mw5oLLjxSNsPRdDgArCZseGEQJVdNYNK5U", u_ctx.peerid);
+	CU_ASSERT_STRING_EQUAL("my3CohS9f57yCqNy4yAPbBRqLaAAJ9oqXV", u_ctx.myid);
+{
+	uint8_t msg[500] = {0};
+	size_t size = 3;
+	int64_t sID0 = 0;
+	CU_ASSERT_NOT_EQUAL(0, UID_formatReqMsg(u_ctx.myid, 31, "Test ECHO", msg, &size, &sID0));
+}
+	uint8_t msg[500] = {0};
+	size_t size = sizeof(msg);
+	int64_t sID0 = 0;
+	CU_ASSERT_EQUAL(0, UID_formatReqMsg(u_ctx.myid, 31, "Test ECHO", msg, &size, &sID0));
+	CU_ASSERT_NOT_EQUAL(sizeof(msg), size);
+	CU_ASSERT_NOT_EQUAL(0, sID0);
+
+	// provider
+	uint8_t fmsg[500] = {0};
+	size_t fsize = sizeof(fmsg);
+	UID_ServerChannelCtx sctx;
+	UID_accept_channel(msg, size, &sctx, fmsg, &fsize);
+
+{
+	char sender[35] = {0};
+	int method = 0;
+	char params[100] = {0};
+	int64_t sID1 = 0;
+	CU_ASSERT_NOT_EQUAL(0, UID_parseReqMsg((uint8_t *)"Foo bar", fsize, sender, sizeof(sender), &method, params, sizeof(params), &sID1));
+}
+	char sender[35] = {0};
+	int method = 0;
+	char params[100] = {0};
+	int64_t sID1 = 0;
+	CU_ASSERT_EQUAL(0, UID_parseReqMsg(fmsg, fsize, sender, sizeof(sender), &method, params, sizeof(params), &sID1));
+
+	CU_ASSERT_STRING_EQUAL(u_ctx.myid, sender);
+	CU_ASSERT_EQUAL(31,method);
+	CU_ASSERT_STRING_EQUAL("Test ECHO", params);
+	CU_ASSERT_EQUAL(sID0, sID1);
+	CU_ASSERT_STRING_EQUAL(sender, sctx.contract.serviceUserAddress);
+
+	CU_ASSERT_NOT_EQUAL(0, UID_checkPermission(method, sctx.contract.profile));
+	CU_ASSERT_EQUAL(0, UID_checkPermission(30, sctx.contract.profile));
+
+	char result[100];
+	CU_ASSERT_EQUAL(0, UID_performRequest(method, params, result, sizeof(result)));
+
+	CU_ASSERT_STRING_EQUAL("UID_echo: <Test ECHO>", result);
+
+	uint8_t response[500] = {0};
+	size_t rsize = sizeof(response);
+	CU_ASSERT_EQUAL(0, UID_formatRespMsg(sctx.contract.serviceProviderAddress, result, 0, sID1, response, &rsize));
+
+	CU_ASSERT_NOT_EQUAL(sizeof(response), rsize);
+
+	CU_ASSERT_EQUAL(0, UID_closeServerChannel(&sctx));
+
+	//user
+	char r_sender[35] = {0};
+	int r_error = -1;
+	char r_result[100] = {0};
+	int64_t sID2 = 0;
+	CU_ASSERT_EQUAL(0, UID_parseRespMsg(response, rsize, r_sender, sizeof(r_sender), &r_error, r_result, sizeof(r_result), &sID2));
+
+	CU_ASSERT_STRING_EQUAL(u_ctx.peerid, r_sender);
+	CU_ASSERT_EQUAL(0, r_error);
+	CU_ASSERT_STRING_EQUAL("UID_echo: <Test ECHO>", r_result);
+	CU_ASSERT_EQUAL(sID1, sID2);
+
+	CU_ASSERT_EQUAL(0, UID_closeChannel(&u_ctx));
+}
+
+
 /************* Test Runner Code goes here **************/
 
 int main ( void )
@@ -151,7 +264,7 @@ int main ( void )
       return CU_get_error();
 
    /* add a suite to the registry */
-   pSuite = CU_add_suite( "identity_test_suite", init_identity_suite, clean_identity_suite );
+   pSuite = CU_add_suite( "identity test suite", init_identity_suite, clean_identity_suite );
    if ( NULL == pSuite ) {
       CU_cleanup_registry();
       return CU_get_error();
@@ -162,6 +275,21 @@ int main ( void )
         (NULL == CU_add_test(pSuite, "test_case_identity2", test_case_identity2)) ||
         (NULL == CU_add_test(pSuite, "test_case_identity3", test_case_identity3)) ||
         (NULL == CU_add_test(pSuite, "test_case_identity4", test_case_identity4))
+      )
+   {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* add a suite to the registry */
+   pSuite = CU_add_suite( "general test suite", init_general_suite, clean_general_suite );
+   if ( NULL == pSuite ) {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* add the tests to the suite */
+   if ( (NULL == CU_add_test(pSuite, "test_case_general1", test_case_general1))
       )
    {
       CU_cleanup_registry();
