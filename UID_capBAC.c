daae513ea15960f87800eb37baf2bb7d7549f662
Marco Palumbi <marco@palumbi.it>
2018-03-08 17:44:34 +0100

    added files for CapBAC

---
+++ UID_capBAC.c
@@ -0,0 +1,14 @@
+/**
+ * @file   UID_capBAC.c
+ *
+ * @date   7/mar/2018
+ * @author M. Palumbi
+ */
+
+
+/**
+ * @file   UID_capBAC.h
+ *
+ * Capability access implementation
+ *
+ */
---
+++ UID_capBAC.h
@@ -0,0 +1,13 @@
+/**
+ * @file   UID_capBAC.h
+ *
+ * @date   7/mar/2018
+ * @author M. Palumbi
+ */
+
+#ifndef __UID_CAPBAC_H
+#define __UID_CAPBAC_H
+
+
+
+#endif
