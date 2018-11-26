3f6263b70dbb7cc0b900b3c618e91cdb8baa4b43
Beatriz84 <bea.formai@gmail.com>
2018-11-26 12:29:27 +0100

    added copyright header

--- UID_transaction.c
+++ UID_transaction.c
@@ -1,4 +1,11 @@
 /*
+ * Copyright (c) 2016-2018. Uniquid Inc. or its affiliates. All Rights Reserved.
+ *
+ * License is in the "LICENSE" file accompanying this file.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+ /*
  * @file   UID_transaction.c
  *
  * @date   12/dec/2016
